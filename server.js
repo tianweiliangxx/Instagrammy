aaafe0d491f2f9010a17a8676f238782bfb5f0b2
闫海亮 <1015951447@qq.com>
2020-05-26 11:44:08 +0800

    first connit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const configure = require('./server/configure')
+
+app = express();
+app = configure(app);
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', function(req, res) {
+    res.send('Hello, World!');
+})
+
+app.listen(app.get('port'), function() {
+    console.log(`Server running on http://localhost:${app.get('port')}`);
+});
